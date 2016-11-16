0605391ff3b3c58e9f99a29e434988a12685ea37
Sharkseven <937257166@qq.com>
2016-11-16 11:34:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
-<?php
-$today = new DateTime();
-echo $today->format('Y-m-d H:i:s');
-echo '<br>';
-echo getenv('APP_ENV');
-echo '<br>';
-echo $_SERVER['APP_ENV'];
-echo '<br>';
-echo phpinfo();
+hello_world
+
